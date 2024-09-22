361148f973b9a9eb3cce7587f910ed62e06ae0f1
francosorbello <franco.sorbelo@gmail.com>
2024-09-22 18:49:36 -0300

    added button to reset camera view

--- player/rigid_player.gd
+++ player/rigid_player.gd
@@ -5,13 +5,11 @@ var move_vector
 @export var game_ui : GameUI
 @export var current_expedition : Expedition
 
-var rotate_joystick : VirtualJoystick
 
 var current_interactable
 
 func _ready() -> void:
     $PlayerCamera.in_use = true
-    rotate_joystick = game_ui.get_rotate_joystick()
 
 func _process(_delta: float) -> void:
     move_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -27,8 +25,9 @@ func _process(_delta: float) -> void:
             current_interactable.interact(self)
             current_interactable = null
 
-    if rotate_joystick and rotate_joystick.is_pressed:
-        $PlayerCamera.rotate_y(-Vector2.RIGHT.dot(rotate_joystick.output) * _delta)
+    if Input.is_action_just_released("reset"):
+        $PlayerCamera.reset()
+        $PlayerCamera.stabilize()
 
 
 func _on_interact_area_area_entered(area:Area3D) -> void:
--- ui/action_button/action_button.gd
+++ ui/action_button/action_button.gd
@@ -6,6 +6,17 @@ enum ActionButtonType {
 }
 
 @export var action : String
+@export var action_type : ActionButtonType = ActionButtonType.TAP
+
 
 func _on_pressed() -> void:
-    Input.action_release(action)
+    if action_type == ActionButtonType.TAP:
+        Input.action_release(action)
+    
+func _on_button_down() -> void:
+    if action_type == ActionButtonType.CONTINUOUS:
+        Input.action_press(action)
+    
+func _on_button_up() -> void:
+    if action_type == ActionButtonType.CONTINUOUS:
+        Input.action_release(action)
