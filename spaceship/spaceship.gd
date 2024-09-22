8d873ce2781d87f8bbe12dbe7b8104dd062a9aa6
francosorbello <franco.sorbelo@gmail.com>
2024-09-22 19:18:15 -0300

    added spaceship interactable

---
+++ spaceship/spaceship.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var food_amount : float = randi_range(30,100)
+var fuel_amount : float = randi_range(0,30)
+
+var already_interacted = false
+@export var current_expedition : Expedition
+
+func _ready():
+    # randomize rotation
+    $Model.rotate(Vector3(0, 1, 0), randf() * 2 * PI)
+
+func can_interact():
+    return not already_interacted
+
+func interact(_rigid_player : RigidPlayer):
+    current_expedition.add_food(food_amount)
+    current_expedition.add_fuel(fuel_amount)
+    already_interacted = true
+    pass
