ea956d4ba5979719165266cb567fd2f4be3357d2
francosorbello <franco.sorbelo@gmail.com>
2024-09-24 19:46:22 -0300

    added player spaceship so players can return from expeditions

--- player_spaceship/player_spaceship.gd
+++ player_spaceship/player_spaceship.gd
@@ -9,9 +9,13 @@ func can_interact():
 
 
 func enable_highlight():
-    pass
-    # $Model.material_override.albedo_color = Color("a6a6a6")
+    _tint_spaceship(Color("a6a6a6")) 
 
 func disable_highlight():
-    # $Model.material_override.albedo_color = Color.WHITE
-    pass
+    _tint_spaceship(Color.WHITE)
+
+func _tint_spaceship(color : Color):
+    var model = $Model as MeshInstance3D
+    for i in range(model.get_surface_override_material_count()):
+        var mat = model.mesh.surface_get_material(i)
+        mat.albedo_color = color
